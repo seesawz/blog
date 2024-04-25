8ba4ba6b8b59ee76b12467b7e3e3da747fc3b521
seesaw_T <seesawgzz@gmail.com>
2024-04-25 23:52:09 +0800

    first push

--- src/script.js
+++ src/script.js
@@ -22,12 +22,19 @@ const camera = new THREE.PerspectiveCamera(75, size.width / size.height, 1, 100)
 scene.add(camera)
 
 
+/* 
+  textures
+*/
+const textureLoader = new THREE.TextureLoader()
+
+const matcapsTexture = textureLoader.load('/matcaps/2.png')
+
 
 const fontLoader = new FontLoader()
 
 fontLoader.load('/fonts/helvetiker_regular.typeface.json',
   (font) => {
-    const textGeometry = new TextBufferGeometry('Hello Three.js',
+    const textGeometry = new TextBufferGeometry('Hello This is seesaw',
       {
         font: font,
         size: 0.5,
@@ -41,12 +48,26 @@ fontLoader.load('/fonts/helvetiker_regular.typeface.json',
         //关于圆角
         bevelSegments: 5
       })
-    const textMaterial = new THREE.MeshBasicMaterial({
-      wireframe: true
+
+    textGeometry.computeBoundingBox()
+    textGeometry.translate(
+      //这里真的很难解释，建议去看12集对应的部分
+      -(textGeometry.boundingBox.max.x - 0.02) * 0.5,
+      -(textGeometry.boundingBox.max.y - 0.02) * 0.5,
+      -(textGeometry.boundingBox.max.z - 0.03) * 0.5
+    )
+
+    //textGeometry.center()
+
+    console.log(textGeometry.boundingBox)
+
+    const textMaterial = new THREE.MeshMatcapMaterial({
+      //wireframe: true
+      matcap: matcapsTexture
     })
     const text = new THREE.Mesh(textGeometry, textMaterial)
     scene.add(text)
-    text.position.x = -1.5
+
   })
 
 
@@ -89,4 +110,3 @@ window.addEventListener('resize', () => {
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
-const gui = new GUI()
